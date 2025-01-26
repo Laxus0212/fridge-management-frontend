10393891f9f1f4e3c18a46b44734cf0ab2dd7e07
Zsolt Váradi <varadi.zsolt@novaservices.hu>
2025-01-26 20:10:02 +0100

    add clear functions to auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import {RoutePaths} from "../enums/route-paths";
+import {CommonService} from './common.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,14 @@ export class AuthService {
   private userFamilyId = 'user_familyId'; // Store family id in localStorage
   private userId = 'userId'; // Store user id in localStorage
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private commonService: CommonService) {}
 
   setUsername(username: string) {
     localStorage.setItem(this.usernameKey, username); // Store username
+  }
+
+  clearUsername() {
+    localStorage.removeItem(this.usernameKey);
   }
 
   getUsername(): string {
@@ -23,6 +28,10 @@ export class AuthService {
 
   setUserFamilyId(familyId: number) {
     localStorage.setItem(this.userFamilyId, familyId.toString()); // Store familyId
+  }
+
+  clearUserFamilyId() {
+    localStorage.removeItem(this.userFamilyId);
   }
 
   getUserFamilyId(): number | null {
@@ -34,6 +43,10 @@ export class AuthService {
     localStorage.setItem(this.userId, userId.toString()); // Store familyId
   }
 
+  clearUserId() {
+    localStorage.removeItem(this.userId);
+  }
+
   getUserId(): number | null {
     const userId = localStorage.getItem(this.userId);
     return userId ? Number.parseInt(userId) : null; // Return null if userId is not found
@@ -41,6 +54,10 @@ export class AuthService {
 
   setToken(token: string) {
     localStorage.setItem(this.tokenKey, token); // Store token
+  }
+
+  clearToken() {
+    localStorage.removeItem(this.tokenKey);
   }
 
   getToken(): string | null {
@@ -52,8 +69,8 @@ export class AuthService {
   }
 
   logout() {
-    //const registerRoute = RoutePaths.Register;
     localStorage.clear(); // Clear all stored data
+    this.commonService.clearUserData();
     void this.router.navigate([RoutePaths.Home]); // Redirect to home page
   }
 }
